d6f9d1c5e255b095544821219ee43a99bfa7aa8d
Quentin Preik <quentin@preik.net>
2025-09-23 09:14:55 -0400

    Add project files.

---
+++ owies/owiesModSystem.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.GameContent;
+namespace owies
+{
+    public class owiesModSystem : ModSystem
+    {
+       
+    }
+}
